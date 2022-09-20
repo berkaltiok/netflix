973fd6f3736fa808b360a777970d4a91c4c0cbb5
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-20 18:33:04 +0300

    docs: add fonts & add sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Sınırsız film, dizi ve çok daha fazlası.
         </h1>
 
         <p className={styles.description}>
