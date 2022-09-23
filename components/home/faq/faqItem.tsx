955904a2890c7d60b19e0d849888f75f4b5d8428
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-24 00:46:40 +0300

    chore: create feature

--- components/home/faq/faqItem.tsx
+++ components/home/faq/faqItem.tsx
@@ -13,7 +13,8 @@ export default function faqItem({ item, onToggle, active }: any) {
         <svg viewBox="0 0 20.5 20.5" focusable="true" style={
           active
             ? { rotate: '180deg' }
-            : { rotate: '45deg' }}>
+            : { rotate: '45deg' }
+        }>
           <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z" />
         </svg>
       </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,42 @@ import Footer from "../components/home/footer";
 import Faq from "../components/home/faq";
 
 const Home: NextPage = () => {
-  const sections = [
-    {component: <Hero />},
-    {component: <Faq />}
+  const Feature = (props: any) => {
+    return (
+      <div className={styles.home__feature} style={props?.reverse ? {flexDirection: "row-reverse"} : {}}>
+        <div className={styles["home__feature-content"]}>
+          <div className={styles["home__feature-title"]}>{props.title}</div>
+          <div className={styles["home__feature-description"]}>{props.description}</div>
+        </div>
+        <div className={styles["home__feature-content"]}>
+          {props.children}
+        </div>
+      </div>
+    )
+  }
+  const list = [
+    { component: <Hero /> },
+    {
+      component:
+        <Feature title="Enjoy on your TV." description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.">
+          <div>Eh baba</div>
+        </Feature>
+    },
+    {
+      component:
+        <Feature reverse={true} title="Enjoy on your TV." description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.">
+          <div>Eh baba</div>
+        </Feature>
+    },
+    { component: <Faq /> },
   ]
 
   return (
     <div className={styles.home}>
       <Header />
       <div className={styles.home__sections}>
-        {sections.map((item, key) =>
-          <div key={key} className={styles["home__sections-card"]}>
+        {list.map((item, key) =>
+          <div key={key} className={styles["home__card"]}>
             {item.component}
           </div>
         )}
