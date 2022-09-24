62e2d26b1c0d041b77900e7a4d07e0481196642c
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-24 20:54:17 +0300

    chore: refactor code

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -5,21 +5,23 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className={styles.header}>
-      <div className={styles['header-logo']}>
-        <Image src="/logo.svg" layout="fill" objectFit='contain' alt="logo" />
-      </div>
-      <div className={styles['header-action']}>
-        <select className={styles['header-action__picker']}>
-          <option>
-            English
-          </option>
-          <option>
-            Türkçe
-          </option>
-        </select>
-        <Link href="/login">
-          <a className={styles['header-action__link']}>Sign In</a>
-        </Link>
+      <div className={styles["header-wrapper"]}>
+        <div className={styles['header-logo']}>
+          <Image src="/logo.svg" layout="fill" objectFit='contain' alt="logo" />
+        </div>
+        <div className={styles['header-action']}>
+          <select className={styles['header-action__picker']}>
+            <option>
+              English
+            </option>
+            <option>
+              Türkçe
+            </option>
+          </select>
+          <Link href="/login">
+            <a className={styles['header-action__link']}>Sign In</a>
+          </Link>
+        </div>
       </div>
     </div>
   )
