451d8542f4643e68cc2478b24e7986072388cf5f
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-24 17:36:33 +0300

    feat: create features for home page

---
+++ components/home/features.tsx
@@ -0,0 +1,57 @@
+import styles from "../../styles/home/Features.module.scss"
+import Image from "next/image"
+
+export function TV() {
+  return (
+    <div className={styles["tv"]}>
+      <div className={styles["tv__image"]}>
+        <Image src="/../public/home/feature/tv/tv.png" width={640} height={480} objectFit="cover" alt="tv image" />
+      </div>
+      <div className={styles["tv__video"]}>
+        <video autoPlay playsInline muted loop>
+          <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  )
+}
+export function Mobile() {
+  return (
+    <div className={styles["mobile"]}>
+      <Image className={styles["mobile__image"]} src="/../public/home/feature/mobile/mobile.jpg" width={640} height={480} objectFit="cover" alt="tv image" />
+      <div className={styles["mobile__card"]}>
+        <div className={styles["mobile__card-image"]}>
+          <Image src="/../public/home/feature/mobile/boxshot.png" width={58} height={80} objectFit="cover" alt="tv image" />
+        </div>
+        <div className={styles["mobile__card-content"]}>
+          <span className={styles["mobile__card-title"]}>Stranger Things</span>
+          <span className={styles["mobile__card-status"]}>Downloading...</span>
+        </div>
+        <div className={styles["mobile__card-animation"]}></div>
+      </div>
+    </div>
+  )
+}
+export function Devices() {
+  return (
+    <div className={styles["devices"]}>
+      <div className={styles["devices__image"]}>
+        <Image src="/../public/home/feature/devices/device-pile.png" width={640} height={480} alt="tv image" />
+      </div>
+      <div className={styles["devices__video"]}>
+        <video autoPlay playsInline muted loop>
+          <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v" type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  )
+}
+export function Kids() {
+  return (
+    <div className={styles["kids"]}>
+      <Image className={styles["kids__image"]} src="/../public/home/feature/kids.png" width={640} height={480} objectFit="cover" alt="tv image" />
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,17 @@ import Hero from "../components/home/hero";
 import styles from '../styles/home/Home.module.scss'
 import Footer from "../components/home/footer";
 import Faq from "../components/home/faq";
+import {TV, Devices, Kids, Mobile} from "../components/home/features"
 
 const Home: NextPage = () => {
   const Feature = (props: any) => {
     return (
       <div className={styles.home__feature} style={props?.reverse ? {flexDirection: "row-reverse"} : {}}>
-        <div className={styles["home__feature-content"]}>
-          <div className={styles["home__feature-title"]}>{props.title}</div>
-          <div className={styles["home__feature-description"]}>{props.description}</div>
+        <div className={styles["home__feature-content"]} style={props?.reverse ? {paddingLeft: "48px"} : {paddingRight: "48px"}}>
+          <h1 className={styles["home__feature-title"]}>{props.title}</h1>
+          <h2 className={styles["home__feature-description"]}>{props.description}</h2>
         </div>
-        <div className={styles["home__feature-content"]}>
+        <div className={styles["home__feature-card"]}>
           {props.children}
         </div>
       </div>
@@ -24,13 +25,25 @@ const Home: NextPage = () => {
     {
       component:
         <Feature title="Enjoy on your TV." description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.">
-          <div>Eh baba</div>
+          <TV />
         </Feature>
     },
     {
       component:
-        <Feature reverse={true} title="Enjoy on your TV." description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.">
-          <div>Eh baba</div>
+        <Feature reverse={true} title="Download your shows to watch offline." description="Save your favorites easily and always have something to watch.">
+          <Mobile />
+        </Feature>
+    },
+    {
+      component:
+        <Feature title="Watch everywhere." description="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.">
+          <Devices />
+        </Feature>
+    },
+    {
+      component:
+        <Feature reverse={true} title="Create profiles for kids." description="Send kids on adventures with their favorite characters in a space made just for them—free with your membership.">
+          <Kids />
         </Feature>
     },
     { component: <Faq /> },
