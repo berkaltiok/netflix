019c572e9dc394af83265f966bd67f4963c2b7db
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-25 23:29:17 +0300

    Revert "fix: hero background importing (TEST)"
    This reverts commit 53e60356ff0d173430f5bab71e792dd4eff1fbc1.

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
    <div className={styles.hero}>
      <div className={styles.hero__background}>
-       <Image src="/_next/image?url=%2F..%2Fpublic%2Fhome%2Fhero.jpg&w=1920&q=75" objectFit="cover" layout="fill" alt="background image"/>
+       <Image src="/../public/home/hero.jpg" objectFit="cover" layout="fill" alt="background image"/>
        <div className={styles["hero__background-filter"]} />
      </div>
      <div className={styles.hero__content}>
