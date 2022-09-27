ab88bca84d599b375679be010e8357a743052759
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-28 00:25:25 +0300

    Chore: refactor code

--- components/home/email.tsx
+++ components/home/email.tsx
@@ -1,16 +1,11 @@
 import {useState} from "react";
 import styles from '../../styles/home/Email.module.scss'
 import * as yup from 'yup';
+import classNames from "classnames";
 
-export default function Email({style}: any) {
+export default function Email({props}: any) {
   let [placeholder, setPlaceholder] = useState(false)
   let [emailValidation, setEmailValidation] = useState("")
-  let placeholderClass = {
-    fontSize: "13px",
-    fontWeight: "600",
-    top: '6px',
-    transform: "translateY(0)"
-  }
   const inputStatues = (e: any) => {
     let inputValue = e.target.value
     let schema = yup.object().shape({
@@ -29,19 +24,28 @@ export default function Email({style}: any) {
           }
         } else setEmailValidation(emailValidation = "")
       });
-    //Placeholder
-    if (inputValue.length > 0) {
-      setPlaceholder(placeholder = true)
-    } else {setPlaceholder(placeholder = false)}
+  }
+  const onFocus = () => {
+    setPlaceholder(placeholder = true)
+  }
+  const offFocus = (e: any) => {
+    if (e.target.value.length === 0) {
+      setPlaceholder(placeholder = false)
+    }
   }
 
   return (
-    <form className={styles.email} style={style}>
+    <form className={styles.email} style={props?.style}>
       <h3 className={styles["email-title"]}>Ready to watch? Enter your email to create or restart your membership.</h3>
       <div className={styles["email-form"]}>
         <label className={styles["email-input"]}>
-          <input onChange={(e) => {inputStatues(e)}} style={emailValidation !== "" ? {borderBottom: "2px solid #ffa00a"} : {}} type="text"/>
-          <span id="placeholder" className={styles["email-input__placeholder"]} style={placeholder ? placeholderClass : {} }>Email address</span>
+          <input
+            onBlur={offFocus}
+            onFocus={onFocus}
+            onChange={(e) => {inputStatues(e)}}
+            style={emailValidation !== "" ? {borderBottom: "2px solid #ffa00a"} : {}}
+            type="text"/>
+          <span id="placeholder" className={classNames(styles["email-input__placeholder"], placeholder ? styles.active : '')}>Email address</span>
           <div className={styles["email-warn"]}>{emailValidation}</div>
         </label>
         <button type="submit" className={styles["email-submit"]}>
