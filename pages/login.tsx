2a21f3f324a38cb5b99aab749dde0ad49fcfe55b
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-28 21:53:03 +0300

    feat: create login page

---
+++ pages/login.tsx
@@ -0,0 +1,22 @@
+import {NextPage} from "next";
+import styles from "../styles/login/Login.module.scss"
+import Image from "next/image"
+import Link from "next/link";
+
+const Login: NextPage = () => {
+  return (
+    <div className={styles.login}>
+      <div className={styles["login-header"]}>
+        <Link href="/">
+          <a className={styles["login-header__logo"]}>
+            <Image draggable={false} src="/logo.svg" alt="logo" layout="fill" />
+          </a>
+        </Link>
+      </div>
+      <form className={styles["login-form"]}>
+        <h1 className={styles["login-form__title"]}>Sign In</h1>
+      </form>
+    </div>
+  )
+}
+export default Login
